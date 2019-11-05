1290aa0068c4875db8de46813a34fd849f2f605b
PavelMarusov <pavelmarusovs@gmail.com>
2019-11-05 14:04:36 +0600

    v/1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static int bossDamage = 50;
     public static String bossDefenceType = "";
     public static int[] heroesHealhs = {250, 250, 250,};
-    public static String heroeName = "Medik";
+    public static String  heroeName = "Medik";
     public static int[] heroesDamages = {20, 20, 20,};
     public static int heroesHealthRecovery = 10;
     public static String[] heroesAttacTypes = {"Physical", "Magical", "Kinetic",};
@@ -20,32 +20,40 @@ public class Main {
     public static void printStatiscics() {
         System.out.println("______________________");
         System.out.println("Boss health" + bossHealh);
-        System.out.println(heroeName +  " " + heroesHealhs[3]);
+//        System.out.println(heroeName[1] + " " + heroesHealhs[4]);
+//        System.out.println(heroeName[0] + " " + heroesHealhs[3]);
+
 
         for (int i = 0; i < heroesAttacTypes.length; i++) {
             System.out.println(heroesAttacTypes[i] + " health " + heroesHealhs[i]);
             System.out.println(heroesHealhs[i] + " health recovery " + heroesHealthRecovery);
 
-        }
-        System.out.println("_________________");
 
-    }
+        }
+
+
+
+        System.out.println("_________________");
+        }
+
+
+
 
     public static void round() {
         changeBossDefence();
         heroesHit();
-        if(bossHealh > 0){
+        if (bossHealh > 0) {
             boosHit();
         }
+        if(heroesHealhs [0] > 200|| heroesHealhs [1] < 200||heroesHealhs [2] < 200){
+            heroesHitPointAp();
+        }
 
-        heroesHitPointAp();
+
 
         printStatiscics();
     }
-    public static int arr (){
-        heroesHealhs = Arrays.copyOf(heroesHealhs,heroesHealhs.length+2);
-        heroesHealhs[3] = 250;
-        return heroesHealhs[3];}
+
 
 
 
@@ -53,7 +61,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        arr();
+
+
 
         printStatiscics();
         while (!isFinished()) {
@@ -67,7 +76,7 @@ public class Main {
             System.out.println("Heroes von");
             return true;
         }
-        if (heroesHealhs[0] <= 0 && heroesHealhs[1] <= 0 && heroesHealhs[2] <= 0) {
+        if (heroesHealhs[0] <= 0 && heroesHealhs[1] <= 0 && heroesHealhs[2] <= 0 && heroesHealhs[3]<=0 && heroesHealhs[4]<=0) {
             System.out.println("Boss von");
             return true;
 
@@ -80,13 +89,17 @@ public class Main {
     public static void boosHit() {
         for (int i = 0; i < heroesHealhs.length; i++) {
             heroesHealhs[i] = heroesHealhs[i] - bossDamage;
-
         }
     }
-    public static void heroesHitPointAp(){
-        if(heroesHealhs [0] < 200 || heroesHealhs[1] < 200 || heroesHealhs [2] < 200 ){
-            for (int i = 0; i <= 2 ; i++) {
-                heroesHealhs[i] =heroesHealhs[i] + heroesHealthRecovery;
+
+    public static void heroesHitPointAp() {
+        if (heroesHealhs[0] < 200 || heroesHealhs[1] < 200 || heroesHealhs[2] < 200 || heroesHealhs[3] < 200 ) {
+            for (int i = 0; i < heroesHealhs.length; i++) {
+                if (i == 3) {
+                    continue;
+
+                }
+                heroesHealhs[i] = heroesHealhs[i] + heroesHealthRecovery;
 
             }
 
@@ -95,6 +108,7 @@ public class Main {
     }
 
     public static void heroesHit() {
+
         for (int i = 0; i < heroesDamages.length; i++) {
             if (!heroesAttacTypes[i].equals(bossDefenceType)) {
                 Random r = new Random();
